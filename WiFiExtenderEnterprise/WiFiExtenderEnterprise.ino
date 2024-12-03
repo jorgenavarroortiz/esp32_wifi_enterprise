cd1fb382f4483a3161245e41c8ca16271757136f
Jorge Navarro-Ortiz <jorgenavarro@ugr.es>
2024-12-03 14:03:11 +0100

    More information (RSSI, channel, ...) given through the serial port

--- WiFiExtenderEnterprise/WiFiExtenderEnterprise.ino
+++ WiFiExtenderEnterprise/WiFiExtenderEnterprise.ino
@@ -1,22 +1,44 @@
 #include <WiFi.h>
 
-#define EAP_IDENTITY "user@ugr.es"         //if connecting from another corporation, use identity@organization.domain in Eduroam
-#define EAP_USERNAME "user@ugr.es"         //oftentimes just a repeat of the identity
-#define EAP_PASSWORD "XXXXXXXX"            //your Eduroam password
-const char *ssid = "eduroam";              // Eduroam SSID
+#define VERSION "1.0"
+
+#define EAP_IDENTITY "user@ugr.es" //if connecting from another corporation, use identity@organization.domain in Eduroam
+#define EAP_USERNAME "user@ugr.es" //oftentimes just a repeat of the identity
+#define EAP_PASSWORD "XXXXXXXX"    //your Eduroam password
+const char *ssid = "eduroam";      // Eduroam SSID
 
 int counter = 0;
+int noSTAs = 0;
+uint32_t ipLastSTA = 0;
 
 #define AP_SSID "AP_SSID"
-#define AP_PASS "XXXXXXXX"
+#define AP_PASS "AP_PASS"
 
 IPAddress ap_ip(192, 168, 4, 1);
 IPAddress ap_mask(255, 255, 255, 0);
 IPAddress ap_leaseStart(192, 168, 4, 2);
-IPAddress ap_dns(8, 8, 4, 4);
+//IPAddress ap_dns(8, 8, 4, 4);
+IPAddress ap_dns(150, 214, 27, 15);
 
 #define TXPOWER WIFI_POWER_2dBm
 
+#if defined(WIRELESS_STICK_V3) || defined (WIRELESS_STICK)
+#define OLED_ENABLED
+#endif
+
+#ifdef OLED_ENABLED
+// For a connection via I2C using the Arduino Wire include:
+#include <Wire.h>               
+#include "HT_SSD1306Wire.h"
+#if defined(WIRELESS_STICK_V3) || defined (WIRELESS_STICK)
+static SSD1306Wire  display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_64_32, RST_OLED); // addr , freq , i2c group , resolution , rst
+#endif
+#endif
+
+
+/////////////////////////////////////////////
+// FUNCTIONS RELATED TO WI-FI CONNECTIVITY //
+/////////////////////////////////////////////
 /*
 // from WiFiGeneric.h:
 typedef enum {
@@ -65,10 +87,44 @@ double convertTxPowerTodBm (int8_t power) {
   }
 }
 
+#ifdef OLED_ENABLED
+////////////////////////////
+// FUNCTIONS RELATED TO OLED
+////////////////////////////
+void VextON(void)
+{
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext, LOW);
+}
+
+void VextOFF(void) //Vext default OFF
+{
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext, HIGH);
+}
+#endif
+
 void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
   Network.onEvent(onEvent);
+
+#ifdef OLED_ENABLED
+    // For OLED
+    VextON();
+    delay(100);
+    // Initialising the UI will init the display too.
+    display.init();
+    display.setFont(ArialMT_Plain_10);
+    // clear the display
+    display.clear();
+    // Show startup message
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.drawString(0, 0,  "WPA-enterprise");
+    display.drawString(0, 10,  "extender v" + String(VERSION));
+    display.drawString(0, 20, "Booting...");
+    display.display();
+#endif
 
   WiFi.AP.begin();
   WiFi.AP.config(ap_ip, ap_ip, ap_mask, ap_leaseStart, ap_dns);
@@ -91,8 +147,16 @@ void setup() {
   }
   Serial.println("");
   Serial.println("WiFi connected");
-  Serial.print("IP address set: ");
-  Serial.println(WiFi.localIP());  //print LAN IP
+  Serial.print("MAC address ");
+  Serial.print(WiFi.macAddress());
+  Serial.print(", IP address ");
+  Serial.print(WiFi.localIP());
+  Serial.print(", netmask ");
+  Serial.print(WiFi.subnetMask());
+  Serial.print(", gateway ");
+  Serial.print(WiFi.gatewayIP());
+  Serial.print(", DNS ");
+  Serial.println(WiFi.dnsIP());
 
   WiFi.setTxPower(TXPOWER);
 }
@@ -101,9 +165,31 @@ void loop() {
   delay(20000);
 
   if (WiFi.status() == WL_CONNECTED) {  //if we are connected to Eduroam network
+#ifdef OLED_ENABLED
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.drawString(0, 0,  "Connected!");
+    if (noSTAs <= 0) {
+      noSTAs = 0;
+      display.drawString(0, 10,  "No STAs");
+    } else {
+      display.drawString(0, 10,  String(noSTAs) + " STAs");
+      display.drawString(0, 20,  IPAddress(ipLastSTA).toString());
+    }
+    display.display();
+#endif
+
     counter = 0;                        //reset counter
-    Serial.println("Wifi is still connected with IP " + String(WiFi.localIP()) + " and transmission power " + String(convertTxPowerTodBm(WiFi.getTxPower())) + " dBm (approx.)");
+    Serial.println("Wifi is still connected with IP " + WiFi.localIP().toString() + " and transmission power " + String(convertTxPowerTodBm(WiFi.getTxPower())) + " dBm (approx.)");
+    Serial.printf("SSID %s, channel %d, RSSI %d dBm\n", WiFi.SSID().c_str(), WiFi.channel(), WiFi.RSSI());
   } else if (WiFi.status() != WL_CONNECTED) {  //if we lost connection, retry
+#ifdef OLED_ENABLED
+    display.clear();
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.drawString(0, 0,  "Disconnected!");
+    display.display();
+#endif
+
     WiFi.begin(ssid);
   }
   while (WiFi.status() != WL_CONNECTED) {  //during lost connection, print dots
@@ -139,11 +225,12 @@ void onEvent(arduino_event_id_t event, arduino_event_info_t info) {
       Serial.println("AP Started");
       Serial.println(WiFi.AP);
       break;
-    case ARDUINO_EVENT_WIFI_AP_STACONNECTED:    Serial.println("AP STA Connected"); break;
-    case ARDUINO_EVENT_WIFI_AP_STADISCONNECTED: Serial.println("AP STA Disconnected"); break;
+    case ARDUINO_EVENT_WIFI_AP_STACONNECTED:    Serial.println("AP STA Connected"); noSTAs++; break;
+    case ARDUINO_EVENT_WIFI_AP_STADISCONNECTED: Serial.println("AP STA Disconnected"); noSTAs--; break;
     case ARDUINO_EVENT_WIFI_AP_STAIPASSIGNED:
       Serial.print("AP STA IP Assigned: ");
-      Serial.println(IPAddress(info.wifi_ap_staipassigned.ip.addr));
+      ipLastSTA = info.wifi_ap_staipassigned.ip.addr;
+      Serial.println(IPAddress(ipLastSTA));
       break;
     case ARDUINO_EVENT_WIFI_AP_PROBEREQRECVED: Serial.println("AP Probe Request Received"); break;
     case ARDUINO_EVENT_WIFI_AP_STOP:           Serial.println("AP Stopped"); break;
